afdeb783f757dc71422a283f809abad98c95cc18
andy <jacodeguy@gmail.com>
2019-07-07 18:36:05 -0500

    rename Stellarium var, fix params, perlMods

---
+++ perlMods.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+use strict;
+use CPAN;
+
+# XSCookies requires Date::Parse ?
+
+my @list = qw(
+common::sense
+Digest::MD5
+Term::ReadLine::Gnu
+File::Temp
+common::sense
+Types::Serialiser
+JSON::XS
+DBI
+DBD::SQLite
+EV
+AnyEvent
+AnyEvent::SerialPort
+AnyEvent::HTTPD
+AnyEvent::Socket
+Text::Xslate
+HTTP::XSCookies
+Text::CSV_XS
+Astro::PAL
+Astro::Coords
+Astro::FITS::CFITSIO
+Starlink::AST
+Udev::FFI
+Net::Curl
+Inline::C
+Inline::CPP
+);
+
+# Astro::FITS::Header  (will install Tk)
+# OpenGL
+# PDL
+
+for my $x (@list)
+{
+	CPAN::Shell->install($x);
+}
+
+
+
