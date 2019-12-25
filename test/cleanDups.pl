3a7b90ffc3de815e9336d43415ab874b825c1ab7
Andy <andy@squeakycode.net>
2019-12-25 16:16:01 -0600

    pct done

--- test/cleanDups.pl
+++ test/cleanDups.pl
@@ -9,6 +9,16 @@ use Simbad;
 my $lookup = Simbad->new();
 
 my $db = $lookup->{db};
+
+my($ttlDups) = $db->selectrow_array(q{
+select count(*) from (
+select catid, id, count(*)
+from lookup
+group by catid, id
+having count(*) > 1
+)});
+print "Dups: $ttlDups\n";
+
 my $q = $db->prepare(q{
 select catalog.catid, catalog.name, tmpx.id
 from catalog
@@ -22,7 +32,7 @@ inner join (
 });
 
 my $r = $db->prepare(q{select starid from lookup where catid = ? and id = ?});
-
+my $loop = 0;
 while (1)
 {
 	$q->execute();
@@ -41,7 +51,7 @@ while (1)
 	#$id = 'J00001575-3010193';
 	#$id = 'J00003704-3011547';
 
-	print "found $catid $cat $id\n";
+	print "found $cat $id\n";
 	$r->execute($catid, $id);
 	my $ttlS = 0;
 	my $ttlL = 0;
@@ -50,8 +60,28 @@ while (1)
 		$ttlS += $db->do("delete from star where starid = $row->[0]");
 		$ttlL += $db->do("delete from lookup where starid = $row->[0]");
 	}
-	print "Delete $ttlS stars, $ttlL lookups\n";
+	print "  Delete $ttlS stars, $ttlL lookups\n";
 	importStar($cat, $id);
+	$loop++;
+	my $sleep = rand(4) + 3;
+	if ($loop >= 10)
+	{
+		$loop = 0;
+		my($done) = $db->selectrow_array(q{
+		select count(*) from (
+		select catid, id, count(*)
+		from lookup
+		group by catid, id
+		having count(*) > 1
+		)});
+		$done = (1 - ($done/$ttlDups)) * 100;
+		printf "  Sleep: %.2f  Pct Done: %.2f\n", $sleep, $done;
+	}
+	else
+	{
+		printf "  Sleep: %.2f\n",$sleep;
+	}
+	sleep($sleep);
 }
 $r = undef;
 $q = undef;
@@ -65,8 +95,6 @@ sub importStar($cat, $id)
 	my $skipped = 0;
 
 	my $result = $lookup->query("around $cat $id radius=20m");
-	my $sleep = rand(6) + 3;
 	$ttl += $result->{saved};
-	printf "saved: %d/$ttl  skipped: %d  web: %.2f  db: %.2f  sleep: %.2f\n", $result->{saved}, $result->{skipped}, $result->{webq}, $result->{saveStar}, $sleep;
-	sleep($sleep);
+	printf "  Saved: %d/$ttl  skipped: %d  web: %.2f  db: %.2f\n", $result->{saved}, $result->{skipped}, $result->{webq}, $result->{saveStar};
 }
